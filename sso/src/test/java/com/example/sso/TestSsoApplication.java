ae3a54255ea4caac338492a9afd7a6f4060175ee
Vladislav <20031911@mail.ru>
2024-04-03 22:09:10 +0300

    feat: добавлен сервис sso

---
+++ sso/src/test/java/com/example/sso/TestSsoApplication.java
@@ -0,0 +1,23 @@
+package com.example.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestSsoApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(SsoApplication::main).with(TestSsoApplication.class).run(args);
+    }
+
+}
