0bb0e2ca2498b7dd6b585490e3698ce73d1f05de
Vladislav <20031911@mail.ru>
2024-05-11 20:34:35 +0300

    feat: добавлен сервис обнаружения и шлюз API

--- products/src/main/java/com/example/products/configurations/SecurityConfig.java
+++ products/src/main/java/com/example/products/configurations/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.products.configurations;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
+@EnableDiscoveryClient
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
--- sso/src/main/java/com/example/sso/configs/SecurityConfig.java
+++ sso/src/main/java/com/example/sso/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.sso.configs;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
+@EnableDiscoveryClient
 public class SecurityConfig {
 
     @Bean
